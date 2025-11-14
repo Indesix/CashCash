61f494ece1f6e38e6a1810182cce815ae58bc896
Indesix <[mathlouthi.ridhouane@gmail.com]>
2025-11-14 22:18:32 +0100

    ajout du schema.prisma

---
+++ src/main/repositories/immeubleRepository.ts
@@ -0,0 +1,42 @@
+import Immeuble from "../../shared/immeuble";
+import { PrismaClient } from "@prisma/client";
+
+export class ImmeubleRepository {
+    private dbclient: PrismaClient;
+    constructor() {
+        this.dbclient = new PrismaClient();
+    }
+    async getImmeubles(): Promise<Immeuble[]> {
+        let immeubles = await this.dbclient.immeuble.findMany();
+        return immeubles;
+    }
+
+    async getImmeubleById(id: number): Promise<Immeuble | null> {
+        return await this.dbclient.immeuble.findUnique({
+            where: { id }
+        });
+    }
+
+    async createImmeuble(immeuble: Omit<Immeuble, 'id'>): Promise<Immeuble> {
+        return await this.dbclient.immeuble.create({
+            data: immeuble
+        });
+    }
+
+    async updateImmeuble(id: number, immeuble: Partial<Omit<Immeuble, 'id'>>): Promise<Immeuble> {
+        return await this.dbclient.immeuble.update({
+            where: { id },
+            data: immeuble
+        });
+    }
+
+    async deleteImmeuble(id: number): Promise<Immeuble> {
+        return await this.dbclient.immeuble.delete({
+            where: { id }
+        });
+    }
+
+    async disconnect(): Promise<void> {
+        await this.dbclient.$disconnect();
+    }
+}
